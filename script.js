e19c09c731472d3020699b577a4d676a178fc84f
TheBMGDiary <140489410+TheBMGDiary@users.noreply.github.com>
2023-07-26 13:52:21 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Grab the entryForm element
+const entryForm = document.getElementById('entryForm')
+const entriesSection = document.querySelector('#entries')
+const entryTextbox = document.querySelector('.entry-textbox')
+const entriesNav = document.querySelector('.entries-nav')
+
+console.log(entryForm)
+console.log(entriesSection)
+
+let entries = 0
+
+// handler to deal with button click
+const handleForm = e => { 
+    event.preventDefault()
+
+    const entryDiv = document.createElement('div')
+    entryDiv.className = 'single-entry'
+    entryDiv.innerHTML = entryTextbox.value
+    entryDiv.style.display = 'none'
+    entriesSection.appendChild(entryDiv)
+    
+    entryTextbox.value = ""
+    const displayEntryButton = document.createElement('button')
+    displayEntryButton.className = 'display-entry-button'
+    displayEntryButton.innerText = ++entries
+    entriesNav.appendChild(displayEntryButton)
+
+    displayEntryButton.addEventListener('click', function() { 
+      const allEntries = document.querySelectorAll('.single-entry')
+      allEntries.forEach(element => element.style.display = 'none')
+      entryDiv.style.display = 'block'
+    })
+  } 
+
+// Add an event listener
+entryForm.addEventListener('submit', handleForm)
