7e09e223b4f97ee4ebd2ed2dd1bcbbd2b16a7868
TheBMGDiary <140489410+TheBMGDiary@users.noreply.github.com>
2023-08-02 14:44:07 +1000

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,19 +1,16 @@
 function auth(event) {
      event.preventDefault();
      
-     
-     
 var username = document.getElementById("userName").value;
 var password = document.getElementById("passWord").value;
-          
-          
-     if (username === "115337" && password === "yes") {
-          window.location.replace("115337.html");
-          return;
-     }
-     
-     if (username === "127667" && password === "ain'tnoway") {
-          window.location.replace("127667.html");
-          return;
-     }
-}    
+
+if ( username == "root" && password == "root") {
+    window.location.replace("115337.html");
+}
+else if( username == "username2" && password == "password2") {
+    window.location.replace("127667.html");
+}
+else {
+    // reject
+     alert('no');
+}
