0e7e1480f7ae0e054b4edeaf73695039d3d7a2e6
TheBMGDiary <140489410+TheBMGDiary@users.noreply.github.com>
2023-08-02 14:18:57 +1000

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,10 @@
+function loginPage(form) {
+if (form.userName.value=="username") {
+if (form.passWord.value=="password") {
+location="115337.html"
+} else {
+alert("Incorrect Password")
+}
+} else {  alert("Incorrect Username")
+}
+}
