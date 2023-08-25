bc0f1801611c8eeb461f2dc681d8105965e33301
AbhijayT <111729735+AbhijayT@users.noreply.github.com>
2023-08-25 11:19:38 +1000

    Update validation.js

--- validation.js
+++ validation.js
@@ -3,14 +3,18 @@ function loginPage(form) {
 var username = document.getElementById("userName").value;
 var password = document.getElementById("passWord").value;
 
+      if (username === "admin@gmail.com" && password === "ADMIN") {
+          window.location.replace("Dashboard.html");
+          return;
+     }
 
      if (username === "115337" && password === "ONG") {
-          window.location.replace("115337.html");
+          window.location.replace("Dashboard.html");
           return;
      }
 
      if (username === "127667" && password === "HEY") {
-          window.location.replace("127667.html");
+          window.location.replace("Dashboard.html");
           return;
      }
 
